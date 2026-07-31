acec99724d959326bf8b8eb175301e6a260b5c9f
Varun Khachane <varunkhachane31@gmail.com>
2026-08-01 04:22:29 +0530

    chore: reorganize testing directory and clean temporary RAP outputs

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: './testing/playwright',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
